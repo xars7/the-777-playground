12d60107d28e2613ab48ca8f660b0d4c487dabd2
Kevin Flynn <96040408+xars7@users.noreply.github.com>
2022-01-03 19:55:03 -0800

    We have a fully functioning script!
    Everything is working as intended and it feels awesome. We are able to create a master password, generate new passwords and store them, and view them. Everything is encrypted so unless the user knows the password they can't access the info. Going to clean everything up do some last finishing touches and we should be good to go!

--- PasswordSafe/PasswordSafe.py
+++ PasswordSafe/PasswordSafe.py
@@ -20,15 +20,132 @@
 #
 #
 
+# Import modules
 import os
 from cryptography.fernet import Fernet
-
+import random
+import string
 
 print("PasswordSafe v1.0.0 by Kevin Flynn(xars7)")
 
-# we want our users to be able to write down their passwords or view them
-#def accessVault():
+def viewPasswords():
+    # when the user chooses this option we want them to be able to view their passwords
+    # question is all the passwords at once, or whichever one they choose?
+    # if we do all the passwords at once it might be troublesome to the user to sift through all the passwords
+    # I think it's best to do the second route
 
+    # get the encryption key
+    with open("PasswordSafe/EncryptionKey.key", "rb") as encKey:
+        key = encKey.read()
+
+    # Create a Fernet object with key
+    f = Fernet(key)
+
+    findPassName = input("Please enter the name of the password you are looking for (Ex. Google) : ")
+    with open("PasswordSafe/PasswordDB/" + findPassName, "rb") as password:
+        thePass = password.read()
+
+    with open("PasswordSafe/UsernameDB/" + findPassName, "rb") as username:
+        theUser = username.read()
+
+    # decrypt the password and username
+    decryptedPass = f.decrypt(thePass)
+    decryptedUser = f.decrypt(theUser)
+
+    decodedPass = decryptedPass.decode()
+    decodedUser = decryptedUser.decode()
+
+
+    print("Your credentials are : " + decodedUser + ":" + decodedPass)
+
+    #
+
+def createPassword():
+    # when the user chooses this option we want them to be able to create a random password for the database
+    # they need to be able to choose the length of the password and wether or not special characters are needed
+    # and then save the login credentials in files named by the user
+
+    # find the encryption key
+    with open("PasswordSafe/EncryptionKey.key", "rb") as encKey:
+        key = encKey.read()
+
+    # create a Fernet object using the key
+    f = Fernet(key)
+    
+    # grab info on password from the user
+    passwordName = input("Please enter the Name you want to call this login : ")
+    passwordUser = input("Please enter the username you want to use for this login : ")
+    enableSpecChar = input("Do you need Special Characters? (y/n) : ")
+    passwordLength = input("Please enter the length of your password : ")
+
+    # if the user needs special characters generate a password with special characters 
+    # if not generate a password without special characters
+    if enableSpecChar == "Y" or enableSpecChar == "y":
+        # defines what letters to use
+        letters = string.ascii_letters + string.digits + string.punctuation
+        # generate the password
+        passw0rd = ''.join(random.choice(letters) for i in range(int(passwordLength)))
+        # print the password
+        print(passw0rd)
+        # encode the username and  password before encryption
+        encodedPass = passw0rd.encode("utf-8")
+        encodedUser = passwordUser.encode('utf-8')
+        # encrypt the username and password
+        encryptedPass = f.encrypt(encodedPass)
+        encryptedUser = f.encrypt(encodedUser)
+        # create a password file and store the encrypted password in it
+        with open("PasswordSafe/PasswordDB/" + passwordName, "wb") as password:
+            password.write(encryptedPass)
+        # create a username file and store the encypted username in it
+        with open("PasswordSafe/UsernameDB/" + passwordName, "wb") as username:
+            username.write(encryptedUser)
+    elif enableSpecChar == "N" or enableSpecChar == "n":
+        # defines what letters to use
+        letters = string.ascii_letters + string.digits
+        # generate the password
+        passw0rd = ''.join(random.choice(letters) for i in range(int(passwordLength)))
+        # print the password
+        print(passw0rd)
+        # encrypt the password
+        encryptedPass = f.encrypt(passw0rd)
+        encryptedUser = f.encrypt(passwordUser)
+        # create a password file and store the encrypted password in it
+        with open("PasswordSafe/PasswordDB/" + passwordName, "wb") as password:
+            password.write(encryptedPass)
+        # create a username file and store the encypted username in it
+        with open("PasswordSafe/UsernameDB/" + passwordName, "wb") as username:
+            username.write(encryptedUser)
+
+    
+
+# we want our users to be able to write down their passwords or view them
+def accessVault():
+    # in order for them to view old passwords or encrypt new passwords they need an encryption key
+    # we are going to use the same EncryptionKey.key file from before to encrypt new passwords or
+    # decrypt password they've already written.
+    # everytime they create a password we want to create a new text file labeled with the name of the website
+    # or name of the place they use the credentials for, and place the encrypted password in the file 
+    # it would probably be best to create another folder within the project folder to hold the encryption keys
+    # and encrypted login txts
+    # all they need is the python script and it takes a lot of the eye sore of 20 files in a folder 
+
+    # get encryption key
+    with open("PasswordSafe/EncryptionKey.key", "rb") as encryptKey:
+        key = encryptKey.read()
+    # Create a Fernet object with the key
+    f = Fernet(key)
+
+    # ask the user if they want to create a password or view their passwords
+    vaultMenuChoice = input("1. View my passwords 2. Create a password : ")
+
+    if vaultMenuChoice == "1":
+        # view passwords
+        viewPasswords()
+    elif vaultMenuChoice == "2":
+        # create a password
+        createPassword()
+    else:
+        print("That's not a menu choice")
 
 
 # Create a login with a username and master password
@@ -58,19 +175,15 @@ def createALogin(usernameCreate, masterPasswordCreate):
 
 # login to the password safe using the inputed master password and username
 def loginToSafe(usernameLogin, masterPasswordLogin):
-    print("We got to the login function")
     # read the encryption key
     with open("PasswordSafe/EncryptionKey.key", "rb") as fernetKey:
         key = fernetKey.read()
-        print("We read the key")
     # read the encrypted login to a login file
     with open("PasswordSafe/Login.txt", "rb") as login:
         usernameCheck = login.read()
-        print("We read the login") 
     # read the encrypted MasterPassword to a MasterPassword File
     with open("PasswordSafe/MasterPassword.txt", "rb") as masterPass:
         masterPassCheck = masterPass.read()
-        print("We read the password") 
     
     # create a Fernet object using the key we found earlier
     f = Fernet(key)
@@ -85,8 +198,7 @@ def loginToSafe(usernameLogin, masterPasswordLogin):
 
     # if the credentials add up the program should say it works
     if decodedUserCred == usernameLogin and decodedPassCred == masterPasswordLogin:
-        print("It works!")
-        #accessVault()
+        accessVault()
     else:
         print("wrong login credentials")
 
